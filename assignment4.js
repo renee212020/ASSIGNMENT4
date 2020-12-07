76c8f15107fc4396a32bd46b92a626098b3fe185
renee212020 <71196944+renee212020@users.noreply.github.com>
2020-12-06 22:12:22 -0500

    assignment4

---
+++ assignment4.js
@@ -0,0 +1,13 @@
+var start = new Date();
+
+function stopTime() {
+  var stop = new Date();
+
+  var startseconds = start.getSeconds();
+  var stopseconds = stop.getSeconds();
+  var difference = stopseconds - startseconds;
+
+alert("It has been" +difference+ "seconds it is time to watch a movie");
+
+
+}
