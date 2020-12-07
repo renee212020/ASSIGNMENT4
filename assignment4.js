44fe3494e5ded41e25227f93052b400990a7402e
renee212020 <71196944+renee212020@users.noreply.github.com>
2020-12-07 01:54:11 -0500

    4

--- assignment4.js
+++ assignment4.js
@@ -11,18 +11,130 @@ alert("It has been" +difference+ "seconds it is time to watch a movie");
 }
 
 
-//This will output when a button is pressed
-function therest(){
+//This will output when a button is pressed in the TV Show List
+function strangerthings(){
+alert("Available on Netflix");}
 
-document.getElementById("output1").innerHTML="a description";
-document.getElementById("output2").innerHTML="output Two here";
-document.getElementById("output3").innerHTML="output Three here";
-document.getElementById("output4").innerHTML="output Three here";
-document.getElementById("output5").innerHTML="output Three here";
-document.getElementById("output6").innerHTML="output Three here";
-document.getElementById("output7").innerHTML="output Three here";
-document.getElementById("output8").innerHTML="output Three here";
-document.getElementById("output9").innerHTML="output Three here";
-document.getElementById("output10").innerHTML="output Three here";
+function TheFreshPrince(){
+alert("Available on Netflix & YouTube");}
 
-}
+function TheOffice(){
+alert("Available on Netflix & YouTube");}
+
+function SeventhHeaven(){
+alert("Available on cbs.com");}
+
+function SchittsCreek(){
+alert("Available on Netflix & YouTube");}
+
+function GreysAnatomy(){
+alert("Available on Netflix & YouTube");}
+
+function Merlin() {
+alert("Available on Netflix");}
+
+function GoodDoctor() {
+alert("Available on YouTube");}
+
+function JulieandThePhantoms() {
+alert("Available on Netflix");}
+
+function JaneTheVirgin() {
+alert("Available on Netflix");}
+
+
+//This will output when a button is pressed in the Movie List
+function Marvel(){
+alert("Available on Netflix, Youtube & Disney+");}
+
+function HarryPotter(){
+alert("Available on Netflix, Amazon Prime & YouTube");}
+
+function Jumanji(){
+alert("Available on Netflix, Amazon Prime & YouTube");}
+
+function AlmostChristmas(){
+alert("Available on YouTube");}
+
+function SpiderVerse(){
+alert("Available on Netflix, Amazon Prime & YouTube");}
+
+function JurassicPark(){
+alert("Available on Amazon Prime & YouTube");}
+
+function HomeAlone() {
+alert("Available on Amazon Prime, Disney+ & YouTube");}
+
+function Deadpool() {
+alert("Available on Amazon Prime & YouTube");}
+
+function Godzilla() {
+alert("Available on Amazon Prime & YouTube");}
+
+function Mulan() {
+alert("Available on Amazon Prime, Disney+ & YouTube");}
+
+
+//This will output when a button is pressed in the Anime List
+function GOH(){
+alert("Available on GOGO Anime & Crunchyroll");}
+
+function SaikiK(){
+alert("Available on Netflix, Crunchyroll, GOGO Anime & YouTube");}
+
+function Naruto(){
+alert("Available on Netflix, Amazon Prime, Crunchyroll, GOGO Anime & YouTube");}
+
+function Major(){
+alert("Available on GOGO Anime");}
+
+function CrossGame(){
+alert("Available on GOGO Anime & YouTube");}
+
+function OnePiece(){
+alert("Available on Crunchyroll, GOGO Anime & Netflix");}
+
+function HunterxHunter() {
+alert("Available on Crunchyroll, GOGO Anime & Netflix");}
+
+function KiminiTodoke() {
+alert("Available on Crunchyroll, GOGO Anime & YouTube");}
+
+function Working() {
+alert("Available on GOGO Anime & Crunchyroll");}
+
+function DetectiveConan() {
+alert("Available on GOGO Anime");}
+
+
+//This will output when a button is pressed in the Book List
+
+function GameofThrones(){
+alert("Available on Kindle, Amazon & Indigo");}
+
+function HarryPotter(){
+alert("Available on Kindle, Amazon & Indigo");}
+
+function NarutoM(){
+alert("Available on Kindle, Amazon, Mangahere & Indigo");}
+
+function TheSelection(){
+alert("Available on Kindle, Amazon & Indigo");}
+
+function MazeRunner(){
+alert("Available on Kindle, Amazon & Indigo");}
+
+function ShieldHero(){
+alert("Available on Kindle, Amazon, Mangahere & Indigo");}
+
+function RoyalWe() {
+alert("Available on Kindle, Amazon & Indigo");}
+
+function TATBILB() {
+alert("Available on Kindle, Amazon & Indigo");}
+
+function Shadowhunters() {
+alert("Available on Kindle, Amazon & Indigo");}
+
+function BeautyPop() {
+alert("Available on Kindle, Amazon, Mangahere & Indigo");}
